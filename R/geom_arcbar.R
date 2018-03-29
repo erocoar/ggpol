89e04fffd6467a575f966d1a76de9b349ab31a5f
Frederik Tiedemann <fj.tiedemann@googlemail.com>
2018-03-29 17:25:51 +0200

    fix group options

---
+++ R/geom_arcbar.R
@@ -0,0 +1,58 @@
+#' Create an Arc-Barchart
+#' 
+#' An arc bar diagram that allows for spacing between the individual arc components 
+#' and spans 180 degrees.
+#' 
+#' @section Aesthetics:
+#' geom_arc_bar understands the following aesthetics (required aesthetics are in bold):
+#' 
+#' - **shares**
+#' - **r0** - inner radius
+#' - **r1** - outer radius
+#' - color
+#' - fill
+#' - linetype
+#' - alpha 
+#'   
+#' @inheritParams ggplot2::geom_polygon
+#' @inheritParams ggplot2::stat_identity
+#' @param n The number of
+#' @param sep Separation between the different shares, as a total proportion of pi.
+#' @export
+#' 
+#' @examples
+#' # Generate Data
+#' # Generate Data
+#' bt <- data.frame(
+#'         parties = c("CDU", "CSU", "SPD", "AfD", "FDP", "Linke", "Gruene", "Fraktionslos"),
+#'         seats   = c(200, 46, 153, 92, 80, 69, 67, 2),
+#'         colors  = c("black", "blue", "red", "lightblue", "yellow","purple", "green", "grey"),
+#'         stringsAsFactors = FALSE)
+#' ggplot_parliament2(bt$parties, bt$seats, bt$colors, sep = 0.05)
+
+GeomArcBar <- ggproto("GeomArcBar", GeomPolygon,
+                      default_aes = list(colour = "black", fill = NA, size = 0.5, linetype = 1, alpha = NA)
+)
+
+
+geom_arc_bar <- function(mapping = NULL, data = NULL, stat = "arc_bar",
+                         position = "identity", n = 360, sep = 0.05, na.rm = FALSE,
+                         show.legend = NA, inherit.aes = TRUE, ...) {
+  layer(data = data, 
+        mapping = mapping, 
+        stat = stat, 
+        geom = GeomArcBar,
+        position = position, 
+        show.legend = show.legend, 
+        inherit.aes = inherit.aes,
+        params = list(na.rm = na.rm, n = n, sep = sep, ...))
+}
+
+
+ggplot(bt) + geom_arc_bar(aes(shares = seats, r0 = 5, r1 = 10, fill = parties)) + coord_fixed()
+ggplot() + geom_circle(aes(x = c(3, 5), y = c(1, 5), r = c(2, 4), fill = gl(2, 1))) + coord_fixed()
+
+
+ggplot() + ggforce::geom_circle(aes(x0 = c(3, 5), y0 = c(1, 5), r=c(2, 4), fill = gl(2, 1))) + coord_fixed()
+
+
---
+++ R/stat-arcbar.R
@@ -0,0 +1,58 @@
+StatArcBar <- ggproto("StatArcBar", Stat,
+  required_aes = c("shares", "r0", "r1"),
+  
+  compute_panel = function(data, scales, n = 360, sep = 0.05) {
+    
+    if (all(data$shares >= 1)) {
+      data$shares <- data$shares / sum(data$shares)
+    }
+    
+    if (sep > 0) {
+      data$shares <- data$shares * (1 - sep)
+      sep_shares <- rep(data$shares, c(rep(2, length(data$shares) - 1), 1))
+      sep_shares[seq(2, (length(sep_shares) - 1), 2)] <- sep / (length(data$shares) - 1)
+      
+      cc <- rev(cumsum(c(0, sep_shares * pi)))
+      cc[1] <- pi
+      cc[length(cc)] <- 0
+      cc <- rbind(cc[seq(1, length(cc), 2)], cc[seq(2, length(cc), 2)])
+    } else {
+      cc <- rev(cumsum(c(0, data$shares * pi)))
+    }
+    
+    df <- lapply(1:nrow(data), function(i) {
+      arc_seq <- seq(cc[1, i], cc[2, i], length.out = n)
+      
+      cbind(
+        x = c(
+          cos(arc_seq) * data$r1[i],
+          rev(cos(arc_seq) * data$r0[i])
+        ),
+        y = c(
+          sin(arc_seq) * data$r1[i],
+          rev(sin(arc_seq) * data$r0[i])
+        ),
+        group = i
+      )
+    })
+    
+    df <- do.call(rbind, df)
+    dd <<- df
+    df
+  }
+)
+
+stat_arc_bar  <- function(mapping = NULL, data = NULL, geom = "arc_bar",
+                          position = "identity", n = 360, sep = 0.05, na.rm = FALSE,
+                          show.legend = NA, inherit.aes = TRUE, ...) {
+  layer(
+    stat = StatArcBar,
+    mapping = mapping,
+    data = data, 
+    geom = geom,
+    position = position, 
+    inherit.aes = inherit.aes,
+    show.legend = show.legend,
+    params = list(na.rm = na.rm, n = n, sep = sep, ...)
+  )
+}
