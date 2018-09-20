cf4dac0202f26ba0309c19c937f7ff82f909cd77
erocoar <fj.tiedemann@googlemail.com>
2018-09-20 10:44:37 +0200

    add ggplot2::zeroGrob to facet_share imports and namespace

--- R/facet_share.R
+++ R/facet_share.R
@@ -84,6 +84,7 @@ facet_share <- function(facets, scales = "fixed",
 #' @rdname ggpol-extensions
 #' @format NULL
 #' @usage NULL
+#' @importFrom ggplot2 zeroGrob
 #' @importFrom grid unit convertWidth grobWidth convertHeight grobHeight
 #' @importFrom gtable gtable_matrix gtable_add_col_space gtable_add_row_space gtable_add_grob
 #' @export
